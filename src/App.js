58439dd98ca2dc06ba66f790b046067b17d2a278
Mlarson1 <hey.now.ml@gmail.com>
2021-03-21 02:38:25 -0400

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import "./style.css";
 export default function App() {
   return (
  <div>
-
+      <div class="ui three buttons">
+        <button class="ui active button">One</button>
+        <button class="ui button">Two</button>
+        <button class="ui button">Three</button>
+      </div>
 </div>)
 }
